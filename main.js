e894af56d84c60ff39f08a7acd6a9041f53880f6
Marco Quintella <2om.marco.quintella@gmail.com>
2019-08-30 18:49:03 -0300

    Cronologico

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ const express = require('express')
 const fs = require('fs')
 const Gpio = require('orange-pi-gpio')
 
-const AC1 = new Gpio({
-  pin: 8, ready: () =>
-  {
-    let value = 0
-    setTimeout(() =>
-    {
-      console.log(value ? 'Ligando' : 'Desligando' + ' AC 1')
-      AC1.write(value)
-      value = +!value
-    }, 1000)
-  }
+const AC1 = new Gpio({ pin: 8 })
+
+function pulsoAC1()
+{
+  AC1.write(1)
+  setTimeout(() => { AC1.write(0) }, 1000)
+}
+
+cron.schedule('55 18 * * *', () =>
+{
+  pulsoAC1()
 })
