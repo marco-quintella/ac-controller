27d8b8694271d68621aff89bbf02ebf5ecbfd237
Marco Quintella <2om.marco.quintella@gmail.com>
2019-09-04 11:41:13 -0300

    Using axios

---
+++ src/layouts/Layout.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-layout view="lhh lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title>Ar Condicionados</q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+    <q-footer>
+      <div class="row">
+        <q-btn flat class="col">Aparelhos</q-btn>
+        <q-btn flat class="col">Horários</q-btn>
+        <q-btn flat class="col">Config</q-btn>
+      </div>
+    </q-footer>
+  </q-layout>
+</template>
+
+<script>
+import { openURL } from 'quasar'
+
+export default {
+  name: 'MyLayout',
+  methods: {
+    openURL
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/pages/aparelhos.vue
@@ -0,0 +1,51 @@
+<template>
+  <q-page padding class="q-pt-lg">
+    <div class="row">
+      <div class="col text-center text-h6">AC 1</div>
+      <div class="col-auto">
+        <q-btn-toggle
+          v-model="ac1.status"
+          class="my-custom-toggle"
+          no-caps
+          rounded
+          unelevated
+          toggle-color="primary"
+          color="white"
+          text-color="primary"
+          :options="[
+          {label: 'Ligado', value: true},
+          {label: 'Desligado', value: false}
+        ]"
+        />
+      </div>
+    </div>
+    <div class="row"></div>
+  </q-page>
+</template>
+
+<script>
+import Axios from 'axios'
+
+export default {
+  data () {
+    return {
+      ac1: {
+        status: true
+      },
+      ac2: {
+        status: true
+      },
+      data: {}
+    }
+  },
+  created () {
+    Axios.get('http://189.50.88.218:85/data')
+      .then(result => {
+        this.data = result
+      })
+  }
+}
+</script>
+
+<style>
+</style>
